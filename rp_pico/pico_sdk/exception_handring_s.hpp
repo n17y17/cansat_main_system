5de876d5057c247c6293d4104958c314e588f36e
n17y17 <N17Y17@outlook.com>
2023-09-03 09:28:25 +0900

    エラー処理システムの構築

--- rp_pico/pico_sdk/exception_handring_s.hpp
+++ rp_pico/pico_sdk/exception_handring_s.hpp
@@ -3,59 +3,139 @@
 
 #include <queue>
 #include <unordered_map>
-#include <utility>
+#include <iostream>  // cerr, endl
+#include <string>  // to_string, string(), c_str()
 
-#include "sensor_s.hpp"
+// ゼロ徐算防止
+template<class T> inline T not0(T n) {return (a ? a : (T)1);}
 
 /*
-処理の重要さ
-重要でない処理でエラーが起きた時に，直ちにスキップするために使う
+処理が成功したか
+kSuccess : 成功しました
+kFailure : 失敗しました
+kError : 実行時エラーが発生しました
 */
-enum ProcessImportance
-{
-    kOptional,  // 使わなくても大きな問題はありません
-    kNormal,  // 行ったほうがよい処理です
-    kEssential  // 絶対に必要です
-};
-
-// 処理が成功したか
 enum ProcessResult
 {
+    kSuccess,  // 成功しました
     kFailure,  // 失敗しました
-    kSuccess  // 成功しました
+    kError  // 実行時エラーが発生しました
 };
 
-// 動作状況
+/*
+動作状況
+正常に稼働中 : kWorking
+動かない時も多い : kLittle
+ほとんど動かない : kStopped
+*/
 enum RunState
 {
-    kStoppedAll,  // 完全に動作を停止しています
-    kWorkingLittle,  // 少しだけ動いています
-    kWorkingNormally  // おおむね問題なく動作しています
+    kWorking,  // 正常に稼働中
+    kLittle,  // 動かない時も多い
+    kStopped  // ほとんど動かない
 };
 
-// センサの値を正常に処理できたかを記録し，センサが正常に作動しているかを判断します
-class SensorErrorManagement
+/*
+プログラムの実行状況やエラーを管理します
+*/
+class Exception
 {
-    // センサが正常に動作したかどうかのログを過去何回まで残すか
-    static const unsigned kMaxSensorFunctionLogSize = 100U;
-
-    // センサの値を処理する関数の動作結果
-    static std::unordered_map<Sensor*, std::unordered_map<unsigned, std::pair<ProcessImportance, std::queue<ProcessResult>>>> kSensorFunctionStatus;
-
-    // 各センサが正常に動作しているかをセンサごとに保存
-    static std::unordered_map<Sensor*, bool> kIsSensorNormally;
-
-    // センサの値を処理する関数の動作結果を記録
-    static void RecordFunctionResult(Sensor* sensor_p, unsigned function_id, ProcessImportance importance, ProcessResult is_success)
+    static const uint16_t kMaxStatus = 65535;  // 処理の成功状況を記録する際の最大値
+    static const uint16_t kDefaultStatus = 60000;  // 処理の成功状況を記録する際の初期値
+    static const uint16_t kMinStatus = 0;  // 処理の成功状況を記録する際の最小値
+    static std::unordered_map<Exception*, std::unordered_map<uint8_t, uint16_t>> kStatus;  // 処理の成功状況を記録する変数です
+protected:
+    /*
+    処理の結果を記録します
+    */
+    void RecordResult(uint8_t process_id, ProcessResult result)
     {
-        std::pair<ProcessImportance, std::queue<ProcessResult>> received_function = kSensorFunctionStatus[sensor_p][function_id];
-        received_function.first = importance;
-        received_function.second.push(is_success);
-        if (received_function.second.size() > kMaxSensorFunctionLogSize) received_function.second.pop();
+        uint16_t& this_status = kStatus[this][process_id];
+        switch (result)
+        {
+            case kSuccess:
+            {
+                if (this_status <= (kMaxStatus - 1)) this_status += 1;  // 関数の処理に成功したため加算
+                break;
+            }
+            case kFailure:
+            {
+                if (this_status >= (kMinStatus + 10)) this_status -= 10;  // 関数の処理に失敗したため減算
+                break;
+            }
+            case kError:
+            {
+                if (this_status >= (kMinStatus + 50)) this_status -= 50;  // 関数の処理中にエラーが発生したため大きく減算
+
+                break;
+            }
+            default:
+            {
+                // エラー
+            }
+        }
+        if (this_status < kMinStatus) this_status = kMinStatus;
+        if (this_status > kMaxStatus) this_status = kMaxStatus;
+    }
+    /*
+    処理の動作状況を返します
+    戻り値は，オブジェクトのポインタと自動的に紐づけられています
+    クラス内の処理の一意な通し番号を引数として渡してください
+    */
+    RunState Status(uint8_t process_id)
+    {
+        if (kStatus[this].count(process_id) == 0) kStatus[this][process_id] == kDefaultStatus;  // 新たに動作状況の変数を作る場合は，デフォルトの値で初期化
+        uint8_t success_rate = (kStatus[this][process_id] - kMinStatus) / not0(kMaxStatus - kMinStatus);  // 処理の成功割合 (%)
+        if (success_rate > 70) return kWorking;
+        else if (success_rate > 30) return kLittle;
+        else return kStopped;
+    }
+public:
+    /*
+    オブジェクト内で行われている処理の動作状況を返します
+    戻り値は，オブジェクトのポインタと自動的に紐づけられています
+    */
+    RunState Status()
+    {
+        uint32_t average_status = 0;  // そのクラスに含まれる全動作状況の平均値
+        for (std::pair<const uint8_t, uint16_t> id_and_status : kStatus[this])
+        {
+            average_status += id_and_status.second;
+        }
+        average_status /= kStatus[this].size();
+        uint8_t success_rate = (average_status - kMinStatus) / not0(kMaxStatus - kMinStatus);  // 処理の成功割合 (%)
+        if (success_rate > 70) return kWorking;
+        else if (success_rate > 30) return kLittle;
+        else return kStopped;
     }
 
-    // センサ処理の関数を実行するかどうかを判断して返す
-    static bool
+    /*
+    エラーを記録します
+    file 部分には __FILE__ と書いてください
+    line 部分には __LINE__ と書いてください
+    (コンパイル時に自動でファイル名と行番号に置き換わります)
+    */
+    void RecordError(const char* file, unsigned line, uint8_t process_id, const std::exception& e = std::runtime_error("Unknown error occurred"))
+    {
+        try
+        {
+            std::string message = "!!ERROR  FILE : " + std::string(file) + "  LINE : " + std::to_string(line) + "  ID : " + std::to_string(process_id) + "\n!!       MESSAGE : " + e.what();
+            std::cerr << message << std::endl;
+            // RecordResult(process_id, kError);
+
+            // エラーをログデータに記録
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "!!ERROR  FILE : " << __FILE__ << "  LINE : " << __LINE__ << "/n!!       MESSAGE : " << e.what() << std::endl;
+        }
+        catch (...)
+        {
+            std::cerr << "!!ERROR  FILE : " << __FILE__ << "  LINE : " << __LINE__ << "/n!!       MESSAGE : Unknown error occurred" << std::endl;
+        }
+    }
 };
+const uint16_t Exception::kMaxStatus, Exception::kDefaultStatus, Exception::kMinStatus;
+std::unordered_map<Exception*, std::unordered_map<uint8_t, uint16_t>> Exception::kStatus;
 
 #endif  // CANSAT_MAIN_SYSTEM_RP_PICO_PICO_SDK_EXCEPTION_HANDRING_S_HPP_
