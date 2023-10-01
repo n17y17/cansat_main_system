a86455cfa74a70d02f38758da92fd3a86cb3f395
n17y17 <N17Y17@outlook.com>
2023-10-01 20:23:31 +0900

    使用例を詳しく書き，わかりやすくしました

--- rp_pico/sc00_cpp_example/sc00_cpp_example.cpp
+++ rp_pico/sc00_cpp_example/sc00_cpp_example.cpp
@@ -2,15 +2,45 @@
 
 void setup()
 {
-    stdio_init_all();
+    try
+    {
+        stdio_init_all();  // pico-sdkの初期化
 
-    bme.init(&i2c, 0x52);  // BMEを初期化
+        bme.init(&i2c, 0x52);  // BMEを初期化
 
-    bme.measure();  // 1回測定
-    sc::Altitude0(bme.pressure(), bme.temperature(), 0.0);  // 標高を計算するための基準点の気圧，気温を設定
+        bme.measure();  // BMEで1回測定
+        sc::Altitude0(bme.pressure(), bme.temperature(), 0.0);  // 標高を計算するための基準点の気圧，気温を設定
+    }
+    catch(const std::exception& e)
+    {
+        // エラー時の処理
+        sc::Error(__FILE__, __LINE__, "An error occurred during setup.", e.what());  // セットアップ時にエラーが発生しました
+    }
+}
+
+void loop()
+{
+    try
+    {
+        // センサで計測
+        measure();
+
+        // 測定結果を記録
+        save_log("temperature:%f`C, pressure:%fhPa, humidity:%f%, altitude:%fm\n", temperature, pressure, humidity, altitude);
+
+        // 1s待機
+        sleep_ms(1000);
+    }
+    catch(const std::exception& e)
+    {
+        // エラー時の処理
+        sc::Error(__FILE__, __LINE__, "An error occurred in the loop.", e.what());  // ループ内でエラーが発生しました
+    }
 }
 
 int main()
 {
     setup();
+
+    while(true) loop();
 }
